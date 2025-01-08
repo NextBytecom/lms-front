14920a600ecd7ae532df6fa35322e8ae4606b024
NextBytecom <157526635+NextBytecom@users.noreply.github.com>
2025-01-08 23:33:37 +0530

    Update Layout.js

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FiMenu } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
 import { AiFillCloseCircle } from "react-icons/ai";
